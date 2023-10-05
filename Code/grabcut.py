9e9a2d73f4127d7c7916c5769f584b9a51f5a7a4
Maxwell <maxwell.limingchi@gmail.com>
2023-10-05 15:16:25 +0100

    Added user defined mask and kernel edge finding

--- Code/grabcut.py
+++ Code/grabcut.py
@@ -2,15 +2,23 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import util
+import sys
+
+
+#click on the positions for segmenting, press "Enter" after finishing selecting.
+
 
 RESCALE_FACTOR = 2
 
-# Load the image and resize
+# Load the image and resize, also sharpen
 img = cv2.imread('Test_image/camera1_2023_08_01_17_20_36_936767.jpg')
 dim = (img.shape[1] // RESCALE_FACTOR, img.shape[0] // RESCALE_FACTOR)
 img = cv2.resize(img, dim)
 
-side = img.shape[1] // 100
+kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]]) 
+img = cv2.filter2D(img, -1, kernel)
+
+side = img.shape[1] // 70
 
 fig = plt.figure()
 
@@ -35,13 +43,16 @@ final_seg_img = np.zeros(img.shape,np.uint8)
 
 for rect in rects:
     print(rect)
-    mask = np.zeros(img.shape[:2],np.uint8)
+    mask = util.circle_mask_generation(rect=rect, diameter=side//1.8, img_size=img.shape)
+
+    #showing the mask
+    plt.imshow(mask)
+    plt.show()
     bgdModel = np.zeros((1,65),np.float64)
     fgdModel = np.zeros((1,65),np.float64)
-    mask, bgdModel, fgdModel = cv2.grabCut(img, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
+    mask, bgdModel, fgdModel = cv2.grabCut(img, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_MASK)
     mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
     img_seg = img*mask2[:,:,np.newaxis]
-    print(img_seg.shape)
     final_seg_img += img_seg
 
 plt.imshow(final_seg_img)
--- Code/util.py
+++ Code/util.py
@@ -1,3 +1,84 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
+
 def generate_rect_from_point(x,y,side,type="screw"):
     if type == "screw":
         return (x-side//2, y-side//2, side, side)
+    
+def circle_mask_generation(rect, diameter, img_size, type="screw"):
+    side = rect[2]
+    center = (rect[1] + side//2, rect[0] + side //2)
+    temp = np.zeros(shape=img_size[:2])
+    rect_temp = np.full(shape=(side, side), fill_value=3, dtype=np.uint8)
+    temp[rect[1]:rect[1]+side,rect[0]:rect[0]+side] += rect_temp
+    mask = gen_circle(temp, center, diameter)
+    mask = mask.astype(np.uint8)
+    return mask
+
+def gen_circle(img: np.ndarray, center: tuple, diameter: int) -> np.ndarray:
+
+
+  
+    """
+        Creates a matrix of ones filling a circle.
+    """
+
+    # gets the radious of the image
+    radious  = diameter//2
+
+    # gets the row and column center of the image
+    row, col = center 
+
+    # generates theta vector to variate the angle
+    theta = np.arange(0, 360)*(np.pi/180)
+
+    # generates the indexes of the column
+    y = (radious*np.sin(theta)).astype("int32") 
+
+    # generates the indexes of the rows
+    x = (radious*np.cos(theta)).astype("int32") 
+
+    # with:
+    # img[x, y] = 1
+    # you can draw the border of the circle 
+    # instead of the inner part and the border. 
+
+    # centers the circle at the input center
+    rows = x + (row)
+    cols  = y + (col)
+
+    # gets the number of rows and columns to make 
+    # to cut by half the execution
+    nrows = rows.shape[0] 
+    ncols = cols.shape[0]
+
+    # makes a copy of the image
+    img_copy = copy.deepcopy(img)
+
+    # We use the symmetry in our favour
+    # does reflection on the horizontal axes 
+    # and in the vertical axes
+
+    for row_down, row_up, col1, col2 in zip(rows[:nrows//4],
+                            np.flip(rows[nrows//4:nrows//2]),
+                            cols[:ncols//4],
+                            cols[nrows//2:3*ncols//4]):
+    
+        img_copy[row_up:row_down, col2:col1] = 1
+
+ 
+    return img_copy
+
+# center = (30,40)
+# ones = np.zeros((100, 100))
+# diameter = 30
+
+# circle = gen_circle(ones, center, diameter)
+# plt.imshow(circle)
+# plt.show()
+
+# mask = circle_mask_generation(rect = (25,25,50,50), diameter = 20, img_size = (100,100))
+# print(mask[50,50])
+# plt.imshow(mask)
+# plt.show()
